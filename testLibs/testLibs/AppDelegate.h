22302a8befded810dccaab318bbbc31931b42db1
lcwxq <zypwygzpqnwxq@126.com>
2017-02-17 20:10:09 +0800

    init

---
+++ testLibs/testLibs/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testLibs
+//
+//  Created by liangqi on 17/2/17.
+//  Copyright © 2017年 zyp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
